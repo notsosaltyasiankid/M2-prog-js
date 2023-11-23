1932108ae4282c731dc27296081258f9622c45b1
Alex Bons <36956@ma-web.nl>
2023-11-23 12:42:54 +0100

    kleur

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -5,9 +5,12 @@ class App
         console.log("hello world!")
         let canvas = document.getElementById("canvasid")
         let g = canvas.getContext("2d");
+        g.fillStyle = "beige"
+        g.fillRect(0,0,canvas.width,canvas.height)
+        g.fillStyle = "green"
         g.fillRect(0,0,10,10);
         console.log(canvas)
     }
 }
 let app = new App();
-app.runApplication();
+app.runApplication()
