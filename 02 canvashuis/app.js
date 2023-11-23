12ac86acb4cab73f2565dd73cac6a8ad0b9fbb92
Alex Bons <36956@ma-web.nl>
2023-11-23 12:14:55 +0100

    les_1

---
+++ 02 canvashuis/app.js
@@ -0,0 +1,13 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!")
+        let canvas = document.getElementById("canvasid")
+        let g = canvas.getContext("2d");
+        g.fillRect(0,0,10,10);
+        console.log(canvas)
+    }
+}
+let app = new App();
+app.runApplication();
