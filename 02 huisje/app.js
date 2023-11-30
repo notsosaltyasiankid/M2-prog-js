1ad9ff354a6190df10cbcb77beda23df0b6e3cd7
Alex Bons <36956@ma-web.nl>
2023-11-30 12:25:38 +0100

    raam

--- 02 huisje/app.js
+++ 02 huisje/app.js
@@ -2,17 +2,59 @@ class App
 {
     runApplication()
     {
-        console.log("hello world!")
-        let canvas = document.getElementById("canvasid")
+        console.log("hello world!");
+        let canvas=document.getElementById("canvasId");
         let g = canvas.getContext("2d");
-        g.beginPath();
-        g.moveTo(300,100);
-        g.lineTo(300,700);
-        g.lineTo(700,300);
+        g.beginPath()
+        g.fillStyle = "red";
+        g.moveTo(600,400);
+        g.lineTo(800,300);
+        g.lineTo(700,200);
         g.closePath();
-        g.fill();
-        console.log(canvas)
+        g.stroke();
+        g.fill()
+
+        g.beginPath()
+        g.moveTo(300,100);
+        g.lineTo(200,300);  
+        g.lineTo(600,400);
+        g.lineTo(700,200);
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+
+        g.beginPath()
+        g.fillStyle = "blue";
+        g.moveTo(600,400);
+        g.lineTo(600,600);
+        g.lineTo(200,500);
+        g.lineTo(200,300);
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+        g.beginPath()
+        g.fillStyle = "blue";
+        g.moveTo(600,400);
+        g.lineTo(800,300);
+        g.lineTo(800,500);
+        g.lineTo(600,600);
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+        g.beginPath()
+        g.fillStyle = "red";
+        g.moveTo(250,370);
+        g.lineTo(250,470);
+        g.lineTo(400,500);
+        g.lineTo(400,400);
+        g.closePath();
+        g.stroke();
+        g.fill()
     }
 }
+
 let app = new App();
-app.runApplication()
+app.runApplication();
