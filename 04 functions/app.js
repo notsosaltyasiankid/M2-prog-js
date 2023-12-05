e72d8324090bcff3ba27d0c2d90eba7ba6c1b393
Alex Bons <36956@ma-web.nl>
2023-12-05 12:52:43 +0100

    zelf_func

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -3,20 +3,35 @@ class App
     runApplication()
     {
         console.log("Hello world!");
-        
+        this.runmario();
+        this.Bowserbrul();
+        let y1 = this.nummers(4,4);
+        console.log(y1);
+        this.tekst();
     }
-    newclassfunction()
-    {   
-        console.log("hello world in de nieuweClassfunction")
-    }
-    anotherFunctionWithArguments(aArgument)
+    runmario()
     {
-        console.log("kom maar met je argument");
-        console.log("hier:");
-        console.log(aArgument);
+        console.log("MARIO!!")
+    }
+    Bowserbrul()
+    {
+        let bowser = console.log("BWAHAHA!!!")
+        return bowser
+    }
+    nummers(a,b)
+    {
+        let y = a + b;
+        return y
+    }
+    tekst()
+    {
+        console.log("hey")
     }
 }
 let app = new App();
 app.runApplication();
-app.newclassfunction();
-app.anotherFunctionWithArguments("mijn argument is iets...");
+app.runmario();
+app.Bowserbrul();
+let y1 = app.nummers(4,4);
+console.log(y1);
+app.tekst();
