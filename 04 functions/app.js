9a4aec56db25fb76aac03a2c1f218a965ab96fab
Alex Bons <36956@ma-web.nl>
2023-12-05 11:55:42 +0100

    arguments

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -1,16 +1,22 @@
+function argumentsAreHandy(shoutout)
+{
+console.log("do you want to giva a shoutout");
+console.log(shoutout);
+}
+argumentsAreHandy("super shout out");
+argumentsAreHandy("yoooooo");
+argumentsAreHandy("boy ain't no way");
 function superMooieGlobalFunction()
 {
 console.log("ik ben globaal");
 console.log("dus je mag mij overal aanroepen");
 }
-superMooieGlobalFunction();
-superMooieGlobalFunction();
 class App
 {
     runApplication()
     {
         console.log("Hello world!");
-        superMooieGlobalFunction();
+        
     }
 }
 let app = new App();
