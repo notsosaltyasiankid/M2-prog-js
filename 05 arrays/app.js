192ffdde07e4bc04b72bfd2d77c92f749831202e
Alex Bons <36956@ma-web.nl>
2023-12-14 11:48:20 +0100

    arrayles1

---
+++ 05 arrays/app.js
@@ -0,0 +1,24 @@
+class App
+{
+    runApplication()
+    {
+     
+        console.log("Hello world!");
+        let arr = ["Kenderick lamar", "MF DOOM", "Bbno$"]
+        console.log(arr)
+
+        for (let i = 0; i < arr.length; i++) {
+            const element = arr[i];
+            console.log(element)
+            console.log(i + ": "+ element)
+        }
+        let nummers = [2,5,7]
+        console.log(nummers)
+        for (let i = 0; i < nummers.length; i++) {  
+            const element = nummers[i];
+            console.log(1 + element)
+        }
+    }
+}
+let app = new App();
+app.runApplication();
